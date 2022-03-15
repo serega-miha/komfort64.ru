49bda771b602fe54561f87c13f1e2cd99957a794
serega-miha <serega.miha@gmail.com>
2022-03-15 11:26:55 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    var box=document.getElementsByClassName('box');
+    var btn=document.getElementById('button');
+    for (let i=4;i<box.length;i++) {
+        box[i].style.display = "none";
+    }
+
+    var countD = 10;
+    btn.addEventListener("click", function() {
+        var box=document.getElementsByClassName('box');
+        countD += 10;
+        if (countD <= box.length){
+            for(let i=0;i<countD;i++){
+                box[i].style.display = "block";
+            }
+        }
+
+    })
+}
