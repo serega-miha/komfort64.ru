b8ecbffa5263930594657f9d18a4a8e6ea816051
serega-miha <serega.miha@gmail.com>
2022-03-16 10:47:24 +0400

    меню, хедер, галерея

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 window.onload = function () {
     var box=document.getElementsByClassName('box');
     var btn=document.getElementById('button');
-    for (let i=4;i<box.length;i++) {
+    for (let i=6;i<box.length;i++) {
         box[i].style.display = "none";
     }
 
-    var countD = 10;
+    var countD = 6;
     btn.addEventListener("click", function() {
         var box=document.getElementsByClassName('box');
-        countD += 10;
+        countD += 6;
         if (countD <= box.length){
             for(let i=0;i<countD;i++){
                 box[i].style.display = "block";
