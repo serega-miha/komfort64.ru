861770b0df42ab411f95cba3a3de4d6f6b96e709
serega-miha <serega.miha@gmail.com>
2022-03-16 15:28:36 +0400

    бургер

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 window.onload = function () {
     var box=document.getElementsByClassName('box');
     var btn=document.getElementById('button');
